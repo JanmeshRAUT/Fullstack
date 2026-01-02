783f43c5bdb9530745ffccb00cf3d952ecc48cda
MedTrust Developer <developer@medtrust.ai>
2026-01-03 02:03:25 +0530

    Fix health CHeckpoint3

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000"; // Backend URL (Vercel ENV or Local)
+// export const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000"; // Backend URL (Vercel ENV or Local)
+export const API_BASE = "https://nulliporous-carbolic-lianne.ngrok-free.dev";
 
 export const getTemperature = async () => {
   try {
